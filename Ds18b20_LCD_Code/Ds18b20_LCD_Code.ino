0ee2e3298dc25b7c2bab12162d34120dd6d4ed58
francis.malapitan <148430635+kikoman123@users.noreply.github.com>
2024-04-01 00:32:42 +0800

    Add files via upload

---
+++ Ds18b20_LCD_Code/Ds18b20_LCD_Code.ino
@@ -0,0 +1,30 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16,2);
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);  
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+void setup(void)
+{
+  lcd.init();
+  Serial.begin(9600);
+  sensors.begin();  
+  lcd.begin(16,2);
+  lcd.backlight();
+}
+void loop(void)
+{ 
+  sensors.requestTemperatures(); 
+  lcd.setCursor(0,0);
+  lcd.print("TEMP: ");
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.print((char)223);
+  lcd.print("C");
+  delay(1000);
+  lcd.clear();
+}
